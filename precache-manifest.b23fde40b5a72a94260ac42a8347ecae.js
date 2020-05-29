6fe0875faade5515b14275e9dd9bfcda003d14ee
Sam Ping <samuelping42@gmail.com>
2020-05-28 22:27:51 -0400

    Updates

---
+++ precache-manifest.b23fde40b5a72a94260ac42a8347ecae.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b59557d90c2a1bfe3971e47da2c14ad5",
+    "url": "/personal-website/index.html"
+  },
+  {
+    "revision": "c999f6e10650e4553d11",
+    "url": "/personal-website/static/css/main.58b86dfb.chunk.css"
+  },
+  {
+    "revision": "2a84ea5361d29157a893",
+    "url": "/personal-website/static/js/2.04016c21.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/personal-website/static/js/2.04016c21.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c999f6e10650e4553d11",
+    "url": "/personal-website/static/js/main.7ddbd88a.chunk.js"
+  },
+  {
+    "revision": "f50455ddb69baf523794",
+    "url": "/personal-website/static/js/runtime-main.3a9d5e67.js"
+  },
+  {
+    "revision": "57feb99dfe8c101b7d58493576b6fe86",
+    "url": "/personal-website/static/media/portrait-full.57feb99d.JPEG"
+  },
+  {
+    "revision": "ba17279e54c96d53fd5f49162f8fe116",
+    "url": "/personal-website/static/media/portrait-thumbnail.ba17279e.JPEG"
+  },
+  {
+    "revision": "2cb36afa525f2b37800afb95f0c4a8e2",
+    "url": "/personal-website/static/media/resume.2cb36afa.pdf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.47a3f0667205fa4f85274f2ba09b611c.js"
+  "/personal-website/precache-manifest.b23fde40b5a72a94260ac42a8347ecae.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/personal-website/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
