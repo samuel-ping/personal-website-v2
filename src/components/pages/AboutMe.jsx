9c129ad90f18e8a8a65d7379919a80a8acc2eebe
Sam Ping <samuelping42@gmail.com>
2020-05-29 01:23:50 -0400

    edited about me

--- src/components/pages/AboutMe.jsx
+++ src/components/pages/AboutMe.jsx
@@ -2,6 +2,13 @@ import React from "react";
 import "./AboutMe.css";
 
 const AboutMe = (props) => {
+  const myDescription = {
+    main:
+      "Hi! I'm Samuel Ping, and I'm a freshman at Rutgers University- New Brunswick studying computer science and business. Back in 3rd grade, I wanted to be an inventor when I grew up. I've unconsciously followed that aspiration throughout my life, from spending hours in the middle of my pile of Legos, to enjoying robotics classes, until I took my first introductory computer programming course in 8th grade which sparked my interest in computer science. Recently, I actually realize that building things is my passion. Since then, I've been pursuing my passion for creating new things.",
+    secondary:
+      "Apart from programming, I love playing tennis, acting, and tinkering with computer hardware. I'm also a fan of all things green! Except boogers.",
+  };
+
   const [isVisible, setVisible] = React.useState(false);
   const domRef = React.useRef();
 
@@ -26,19 +33,10 @@ const AboutMe = (props) => {
         </div>
         <hr className="divider" />
         <div className="description-mobile">
-          Hi! I'm Samuel Ping, and I'm a freshman at Rutgers University- New
-          Brunswick studying computer science and business. Back in 3rd grade, I
-          wanted to be an inventor when I grew up. I've unconsciously followed
-          that passion throughout my life, from spending hours in the middle of
-          my pile of Legos, to enjoying robotics classes, until I took my first
-          introductory computer programming course in 8th grade which sparked my
-          interest in computer science. Since then, I've been pursuing my
-          passion for creating new things by coding.
+          {myDescription.main}
           <br />
           <br />
-          Apart from programming, I love playing tennis, acting, and tinkering
-          with computer hardware. I'm also a fan of all things green! Except
-          boogers.
+          {myDescription.secondary}
         </div>
       </div>
     );
@@ -50,19 +48,10 @@ const AboutMe = (props) => {
         </div>
         <div className={isVisible ? "vertical-divider" : undefined} />
         <div className={isVisible ? "description" : undefined}>
-          Hi! I'm Samuel Ping, and I'm a freshman at Rutgers University- New
-          Brunswick studying computer science and business. Back in 3rd grade, I
-          wanted to be an inventor when I grew up. I've unconsciously followed
-          that passion throughout my life, from spending hours in the middle of
-          my pile of Legos, to enjoying robotics classes, until I took my first
-          introductory computer programming course in 8th grade which sparked my
-          interest in computer science. Since then, I've been pursuing my
-          passion for creating new things by coding.
+          {myDescription.main}
           <br />
           <br />
-          Apart from programming, I love playing tennis, acting, and tinkering
-          with computer hardware. I'm also a fan of all things green! Except
-          boogers.
+          {myDescription.secondary}
         </div>
       </div>
     );
