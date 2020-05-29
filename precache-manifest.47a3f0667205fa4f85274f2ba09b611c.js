8f47e2016d8c12b2ccb90bbe36efbeed5bf1edb7
Sam Ping <samuelping42@gmail.com>
2020-05-28 22:22:26 -0400

    Updates

---
+++ precache-manifest.47a3f0667205fa4f85274f2ba09b611c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ab53b4d508f9e4782128f168490594c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a7d70112bfad690818ed",
+    "url": "/static/css/main.58b86dfb.chunk.css"
+  },
+  {
+    "revision": "2a84ea5361d29157a893",
+    "url": "/static/js/2.04016c21.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.04016c21.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7d70112bfad690818ed",
+    "url": "/static/js/main.bd280114.chunk.js"
+  },
+  {
+    "revision": "4ac598fa20d7619937c8",
+    "url": "/static/js/runtime-main.90834230.js"
+  },
+  {
+    "revision": "57feb99dfe8c101b7d58493576b6fe86",
+    "url": "/static/media/portrait-full.57feb99d.JPEG"
+  },
+  {
+    "revision": "ba17279e54c96d53fd5f49162f8fe116",
+    "url": "/static/media/portrait-thumbnail.ba17279e.JPEG"
+  },
+  {
+    "revision": "2cb36afa525f2b37800afb95f0c4a8e2",
+    "url": "/static/media/resume.2cb36afa.pdf"
+  }
+]);
